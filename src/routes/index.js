15e85df4990726e0ab0983fc75d9a040345cd0dc
jiangwf <18128878142qq.com>
2017-09-28 20:02:15 +0800

    完善迁移

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+/*import React from 'react';
+import {Router,Rute,IndexRoute}from 'react-route';
+const routes = (
+    <Route>
+        <Route component={App}>
+			<Route component={Home} path="/"></Route>
+			<Route component={Image} path="/image"></Route>
+		</Route>
+    </Route>
+);*/
